70816537dd0cdc8932e0540fc62a938f679a51ed
Guilherme Lopes <guilhermelopesdonascimento@hotmail.com>
2020-01-22 11:30:26 -0300

    Inserindo Pedido

--- src/main/java/com/guilherme/cursomc/CursomcApplication.java
+++ src/main/java/com/guilherme/cursomc/CursomcApplication.java
@@ -16,7 +16,7 @@ import com.guilherme.cursomc.domain.Estado;
 import com.guilherme.cursomc.domain.ItemPedido;
 import com.guilherme.cursomc.domain.Pagamento;
 import com.guilherme.cursomc.domain.PagamentoComBoleto;
-import com.guilherme.cursomc.domain.PagamentoComCartão;
+import com.guilherme.cursomc.domain.PagamentoComCartao;
 import com.guilherme.cursomc.domain.Pedido;
 import com.guilherme.cursomc.domain.Produto;
 import com.guilherme.cursomc.domain.enums.EstadoPagamento;
@@ -133,7 +133,7 @@ public class CursomcApplication implements CommandLineRunner {
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
 		
-		Pagamento pagto1 = new PagamentoComCartão(null, EstadoPagamento.QUITADO, ped1, 6);
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
 		
 		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
--- src/main/java/com/guilherme/cursomc/services/PedidoService.java
+++ src/main/java/com/guilherme/cursomc/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.guilherme.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.guilherme.cursomc.domain.ItemPedido;
+import com.guilherme.cursomc.domain.PagamentoComBoleto;
 import com.guilherme.cursomc.domain.Pedido;
+import com.guilherme.cursomc.domain.enums.EstadoPagamento;
+import com.guilherme.cursomc.repositories.ItemPedidoRepository;
+import com.guilherme.cursomc.repositories.PagamentoRepository;
 import com.guilherme.cursomc.repositories.PedidoRepository;
 import com.guilherme.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -14,9 +21,42 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstance(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstance());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
