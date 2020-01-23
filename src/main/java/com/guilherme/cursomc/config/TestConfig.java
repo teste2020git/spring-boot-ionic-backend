4cc0d2719ecd23d836fe9ad530f0655deed41bc2
Guilherme Lopes <guilhermelopesdonascimento@hotmail.com>
2020-01-23 17:32:59 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method

--- src/main/java/com/guilherme/cursomc/config/TestConfig.java
+++ src/main/java/com/guilherme/cursomc/config/TestConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.guilherme.cursomc.services.DBService;
+import com.guilherme.cursomc.services.EmailService;
+import com.guilherme.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -20,5 +22,10 @@ public class TestConfig {
 	public boolean instantiateDatabase() throws ParseException {
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 }
---
+++ src/main/java/com/guilherme/cursomc/services/EmailService.java
@@ -0,0 +1,12 @@
+package com.guilherme.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.guilherme.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+}
