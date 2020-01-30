6888217e0ae12ee3c10972ea8ba6aeb5fcd801de
Guilherme Lopes <guilhermelopesdonascimento@hotmail.com>
2020-01-30 17:23:47 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/guilherme/cursomc/CursomcApplication.java
+++ src/main/java/com/guilherme/cursomc/CursomcApplication.java
@@ -1,18 +1,25 @@
 package com.guilherme.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.guilherme.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\desen\\Documents\\AmbienteEstudo\\Imagens\\ana.jpg");
 	}
 
 }
