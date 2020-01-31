90d9739582e9665683659dcead580ff5b8c36fed
Guilherme Lopes <guilhermelopesdonascimento@hotmail.com>
2020-01-31 17:14:42 -0300

    Expondo o header Authorization

--- src/main/java/com/guilherme/cursomc/resources/AuthResource.java
+++ src/main/java/com/guilherme/cursomc/resources/AuthResource.java
@@ -31,6 +31,7 @@ public class AuthResource {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("acess-control-expose-headers", "Authorization");
 		return ResponseEntity.noContent().build();
 	}
 	
